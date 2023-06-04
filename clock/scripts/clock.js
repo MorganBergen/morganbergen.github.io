9d93bf6cbe425f8706eb4703f97523efbbe4de7b
morgan bergen <morgan@ku.edu>
2023-06-04 15:46:36 -0500

    removed dir

--- clock/scripts/clock.js
+++
@@ -1,9 +0,0 @@
-
-function displayTime() {
-    let clock = document.querySelector('#clock');
-    let now = new Date();
-    clock.textContent = now.toLocaleTimeString();
-}
-
-displayTime();
-setInterval(displayTime, 1000);
