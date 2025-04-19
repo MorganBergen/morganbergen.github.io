35e57cbbeac167fcfe8bf158c294a2e53f54634d
morgan bergen <morganmahabergen@icloud.com>
2025-04-19 18:33:26 -0500

    added next configuration constant

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} **/
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export',  // Enable static exports
+  basePath: '',      // Base path for your site
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}
 
 export default nextConfig;
