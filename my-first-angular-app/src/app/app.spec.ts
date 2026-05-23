afe47ea3befb45199b6bdc47ec599490952ff64a
Morgan Bergen <morganmahabergen@icloud.com>
2026-05-23 16:40:23 -0500

    update pwd

--- my-first-angular-app/src/app/app.spec.ts
+++ my-first-angular-app/src/app/app.spec.ts
@@ -14,10 +14,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the Matrix canvas', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-angular-app');
+    expect(compiled.querySelector('canvas.matrix-canvas')).toBeTruthy();
   });
 });
